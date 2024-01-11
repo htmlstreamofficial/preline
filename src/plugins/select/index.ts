5edee4a47887c892999cf25117618e5748293796
animena <59225072+animena@users.noreply.github.com>
2024-01-11 13:06:59 +0000

    check if there is value/option already selected before placing the placeholder in the input, to avoid the issue where it appears in the background of the input

--- src/plugins/select/index.ts
+++ src/plugins/select/index.ts
@@ -364,8 +364,10 @@ class HSSelect extends HSBasePlugin<ISelectOptions> implements ISelect {
 
 	private buildTagsInput() {
 		this.tagsInput = document.createElement('input');
-		(this.tagsInput as HTMLInputElement).placeholder = this.placeholder;
 
+		if (!this.value.length)
+			(this.tagsInput as HTMLInputElement).placeholder = this.placeholder;
+		
 		if (this.tagsInputClasses)
 			classToClassList(this.tagsInputClasses, this.tagsInput);
 
